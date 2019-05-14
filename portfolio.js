2253db60111f72bb45397d4b99eee7957026a567
CN <cn@Codes-MacBook-Pro.local>
2019-05-14 16:12:08 +0100

    Portfolio

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+$(function(){
+	setTimeout(function(){
+		$(".flyintext");
+	},500);	
+});
+
+$(document).ready(function() {
+  $(".title").lettering();
+  $(".button").lettering();
+});
+
+
+
+
+
+$(document).ready(function() {
+  animation();
+}, 1000);
+
+$('.button').click(function() {
+  animation();
+});
+
+
+  
